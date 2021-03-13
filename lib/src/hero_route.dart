f54eb0191c06fd55e071ece8c1e3128a4e8dd6e4
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-03-13 15:14:21 +0100

    Initial Commit

---
+++ lib/src/hero_route.dart
@@ -0,0 +1,45 @@
+// NOTE: Most of the credit for this code goes to 'funwithflutter' on GitHub
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class HeroDialogRoute<T> extends PageRoute<T> {
+  HeroDialogRoute({
+    @required WidgetBuilder builder,
+    RouteSettings settings,
+    bool fullscreenDialog = false,
+  })  : _builder = builder,
+        super(settings: settings, fullscreenDialog: fullscreenDialog);
+
+  final WidgetBuilder _builder;
+
+  @override
+  bool get opaque => false;
+
+  @override
+  bool get barrierDismissible => true;
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 300);
+
+  @override
+  bool get maintainState => true;
+
+  @override
+  Color get barrierColor => Colors.black54;
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
+
+  @override
+  Widget buildPage(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation) {
+    return _builder(context);
+  }
+
+  @override
+  String get barrierLabel => 'Open Popup Dialog';
+}
